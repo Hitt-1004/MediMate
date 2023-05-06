b86964df5e5e6eac46b81081f653b9d9822dd7ed
Hitt-1004 <114856805+Hitt-1004@users.noreply.github.com>
2023-05-06 10:10:24 +0530

    Update dbconfig.js

--- Backend/config/dbconfig.js
+++ Backend/config/dbconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: 'yoursecret',
-    database: 'mongodb+srv://demo_user:mpnbs590A@cluster0.fbjx6bd.mongodb.net/test?retryWrites=true&w=majority',
+    database: 'mongodb+srv://user:password@cluster0.fbjx6bd.mongodb.net/test?retryWrites=true&w=majority',
     collection: 'patients'
 }
